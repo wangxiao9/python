8d502e9a4500327ae2317552c474b36cb6cdcddd
z0040utb <xiao.wang.ext@siemens.com>
2019-03-06 15:55:55 +0800

    logger.py

--- closure/logger.py
+++ closure/logger.py
@@ -2,4 +2,24 @@
 # @Time    : 2019/1/30 15:33
 # @Author  : WANGXIAO
 
-print('hello world')
+import logging
+
+
+def log_header(logger_name):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s [%(name)s] %(levelname)s  %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    logger = logging.getLogger(logger_name)
+
+    def _logging(log,level):
+        if level == 'debug':
+            logger.debug(log)
+        elif level == 'warning':
+            logger.warning(log)
+        elif level == 'error':
+            logger.error(log)
+        else:
+            raise Exception('nothing logger')
+    return _logging
